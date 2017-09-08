b7cbec201458a3c4a2fe9859a904bca92474b832
NIC\rpolanco <cops@nic.mx>
2017-09-08 13:05:20 -0500

    Inicialización del repositorio del cliente .NET

---
+++ InsignaWsClient/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using InsignaWsClient.InsignaWs;
+using System.ServiceModel;
+using System.Net;
+using System.ServiceModel.Channels;
+
+namespace InsignaWsClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creamos el proxy que servirá como cliente y le especificamos el endpoint al cual debe apuntar
+            InsignaSOAPWSClient clientProxy = new InsignaSOAPWSClient();
+            //Se configura el usuario y contraseña del usuario de Insigna
+            clientProxy.ClientCredentials.UserName.UserName = "usuario";
+            clientProxy.ClientCredentials.UserName.Password = "password";
+            try
+            {
+                //Se llama a los métodos del Servicio web mediante las clases que importamos
+                cfdiInfoWrapper wrapper = new cfdiInfoWrapper();
+                wrapper.uuid = "905b5fec-720e-480c-b679-61d8a3d7c71a";
+                CfdiInfoResult result = clientProxy.getCfdiInfo(wrapper);
+                Console.WriteLine("Response Code: {0} " + result.responseCode);
+                Console.WriteLine("Response Description: {0} " + result.responseDescription);
+                Console.WriteLine("Request Date {0}" + result.requestDate);
+                Console.WriteLine("Response Date: {0} " + result.responseDate);
+                Console.WriteLine("Server transaction ID: {0} " + result.serverTransactionId);
+                Console.WriteLine("Transaction ID: {0} " + result.transactionId);
+                Console.WriteLine("Execution Time: {0} " + result.executionTime);
+                Console.WriteLine("CfdiStatus: {0} " + result.cfdiStatus);
+                Console.WriteLine("--------------------------------------------------");
+                Console.WriteLine(result.cfdiStatus);
+                Console.ReadLine();
+            }
+            catch (FaultException<operationFailed> e)
+            {
+                Console.WriteLine("Código de error: " + e.Detail.errorCode);
+                Console.WriteLine("Descripción del error: " + e.Detail.errorDescription);
+
+                Console.ReadLine();
+            }
+            catch (FaultException e)
+            {
+                Console.WriteLine("Error en la petición: " + e.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ InsignaWsClientVB/Module1.vb
@@ -0,0 +1,43 @@
+﻿Imports System.ServiceModel
+
+Module Module1
+
+    Sub Main()
+        System.Net.ServicePointManager.ServerCertificateValidationCallback = AddressOf AcceptAllCertifications
+        'Creamos el proxy que servirá como cliente y le especificamos el endpoint al cual debe apuntar
+        Dim clientProxy As InsignaWs.InsignaSOAPWSClient
+        clientProxy = New InsignaWs.InsignaSOAPWSClient
+        Dim wrapper As InsignaWs.cfdiInfoWrapper
+        wrapper = New InsignaWs.cfdiInfoWrapper
+        Dim result As InsignaWs.CfdiInfoResult
+        result = New InsignaWs.CfdiInfoResult
+
+        Try
+            'Indicamos las credenciales con las que se idenficiará el cliente en el servicio web
+            clientProxy.ClientCredentials.UserName.UserName = "usuario"
+            clientProxy.ClientCredentials.UserName.Password = "password"
+            'Se llama a los métodos del Servicio web mediante las clases que importamos
+            wrapper.uuid = "753e9e17-39f2-44f1-91fe-c7c0bec617a0"
+            result = clientProxy.getCfdiInfo(wrapper)
+            Console.WriteLine("Response Code: {0} " + result.responseCode)
+            Console.WriteLine("Response Description: {0} " + result.responseDescription)
+            Console.WriteLine("Execution Time: {0} " + result.executionTime)
+            Console.WriteLine("Request Date {0}" + result.requestDate)
+            Console.WriteLine("Response Date: {0} " + result.responseDate)
+            Console.WriteLine("Server transaction ID: {0} " + result.serverTransactionId)
+            Console.WriteLine("Transaction ID: {0} " + result.transactionId)
+            Console.WriteLine("CfdiStatus: {0} " + result.cfdiStatus)
+
+            Console.ReadLine()
+        Catch ex As FaultException(Of InsignaWs.operationFailed)
+            ' Si ocurre algún error se imprime el código y la descripción
+            Console.WriteLine("Código de error: " + ex.Detail.errorCode.ToString)
+            Console.WriteLine("Descripción del error: " + ex.Detail.errorDescription)
+            Console.ReadLine()
+        End Try
+    End Sub
+    Public Function AcceptAllCertifications(ByVal sender As Object, ByVal certifications As System.Security.Cryptography.X509Certificates.X509Certificate,
+                                            ByVal chain As System.Security.Cryptography.X509Certificates.X509Chain, ByVal sslPolicyErros As System.Net.Security.SslPolicyErrors) As Boolean
+        Return True
+    End Function
+End Module
